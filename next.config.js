4a0aa0c058320b836c6a4c1b89fe86a49c267ff7
Ali TEK <alitek@gmail.com>
2024-01-18 12:49:33 +0300

    updated

--- next.config.js
+++ next.config.js
@@ -1,15 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+
+  reactStrictMode: false,
+  // swcMinify: true,
   appDir: true,
   publicRuntimeConfig: {
     // Will be available on both server and client
     staticFolder: "/static",
   },
   
-  cleanDistDir: true,
-  output:"standalone",
+  // cleanDistDir: true,
+  // output:"standalone",
   poweredByHeader: false,
   trailingSlash: false,
   // headers: { 'developed-by': 'ali tek' },
