aac54a2a61e8eae2294d06d7cc7b19fff93d0626
Ali TEK <alitek@gmail.com>
2024-01-26 16:57:38 +0300

    updated

--- src/app/install/page.tsx
+++ src/app/install/page.tsx
@@ -1,16 +1,15 @@
 'use client'
 
 import React, { FC, useState, useEffect } from 'react'
-import AddToHomeScreen from './AddToHomeScreen/AddToHomeScreen'
 import Link from 'next/link'
 import useUserAgent from './useUserAgent'
 
 export interface PageInstallProps { }
 
-const PageInstall: FC<PageInstallProps> = ({ }) => {
+const PageInstall: FC<PageInstallProps> = async ({ }) => {
   const [welcomeMessage, setWelcomeMessage] = useState<string>('Checking device...')
   const { isMobile, userAgentString, userAgent } = useUserAgent()
-
+  
   useEffect(() => {
     const welcomeMessage = isMobile ? 'You are on a mobile device.' : 'You are on a desktop device. Please use a mobile device to view this app.'
     setWelcomeMessage(welcomeMessage)
@@ -25,9 +24,8 @@ const PageInstall: FC<PageInstallProps> = ({ }) => {
         {/* <div className='max-w-md mx-auto space-y-6'> */}
         <div className='w-full'>
           <p className='text-2xl'>{welcomeMessage}</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt esse fugit dicta iusto blanditiis numquam, odit repudiandae, recusandae nemo natus, veritatis quidem itaque adipisci sed alias tempore consequatur officia distinctio!</p>
           {userAgentString && <p className="text-center text-xs text-gray-400">{userAgentString}</p>}
-        <AddToHomeScreen />
+
         </div>
       </div>
     </div>
